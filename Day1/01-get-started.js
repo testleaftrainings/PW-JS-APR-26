a99cb5329c92439cb494c11282a99d61ee4c4124
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2026-04-26 20:30:59 +0530

    Add files via upload

---
+++ Day1/01-get-started.js
@@ -0,0 +1,11 @@
+
+
+/* console.log("Step1")
+//setTimeout(()=>{console.log("Step2")},3000)
+console.log("Step3") */
+
+//javascript engine(browser) === nodejs (VS code) => V8 Engine
+
+// This is single line commenting
+
+//Multiple lines to be comented Alt+Shift+a 
